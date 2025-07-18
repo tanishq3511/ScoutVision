242cbf712050c4d1695be86318c1abba4b0636ed
Tanishq Akasapu <tanishq3511@gmail.com>
2025-07-17 19:02:33 -0500

    Delete scout-vision/src/cognito/UserPool.ts
    ignore cognito pt 2

--- scout-vision/src/cognito/UserPool.ts
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
-
-const poolData = {
-    UserPoolId: "us-east-2_PkoPLGONz",
-    ClientId: "592pj6sif6nbbhkb67ksdt3iv8"
-}
-
-export default new CognitoUserPool(poolData);
